e0383b97b59b08ed9c12195afd96dbf9f677ec83
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-09-23 11:47:03 +0300

    Pabeigta klase Aplis

--- src/uzdevums5/Aplis.java
+++ src/uzdevums5/Aplis.java
@@ -1,5 +1,30 @@
 package uzdevums5;
 
-public class Aplis {
+	public class Aplis {
+		public Centrs punkts;
+		public double radiuss;
 
+		public Aplis (Centrs p, double r) {
+			punkts = p; 
+			radiuss = r;
 }
+
+		public String izvaditAplaInfo() {
+			return "Rādiuss: "+radiuss;
+}
+
+		//izveidot metodi rinka linijas aprekinasanai 
+		public double rLGarums() {
+			double c = 2 *Math.PI*radiuss;
+			return Math.round(c*100.0)/100.0;
+		}
+		
+		//rinka laukuma aprekinasanai
+		public double rLaukums() {
+			double s = Math.PI*Math.pow(radiuss, 2);
+			return Math.round(s*100.0)/100.0;
+		}
+		
+		
+		
+}		
