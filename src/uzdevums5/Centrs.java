8921e5f22e16b63b2df7af15f69ce648f7fde224
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-09-23 11:20:47 +0300

    Pabeigta klase Punkts

---
+++ src/uzdevums5/Centrs.java
@@ -0,0 +1,26 @@
+package uzdevums5;
+
+public class Centrs {
+	//Atributi 
+	public int y, x;
+	
+	//Konstruktors
+	public Centrs (int x, int y) {
+		this.x= x;
+		this.y = y;
+		}
+	
+	//Metodes 
+	public String izvaditKoordinatas() {
+		return "Centra punkts atrodas :\n x = "+x+" y = "+y;
+	}
+	
+	public void parvietot (int koordX, int koordY) {
+		x += koordX;
+		y += koordY;
+	}
+	
+	
+	
+	
+}
