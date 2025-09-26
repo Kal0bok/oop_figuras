a2ce990e9459122333f86b400726aa1c46fcb18f
Bogdans <bogdans.cumaks@sk.lvt.lv>
2025-09-26 09:26:59 +0300

    Izveidoti figuras

--- src/uzdevums5/Figuras.java
+++ src/uzdevums5/Figuras.java
@@ -1,8 +1,19 @@
 package uzdevums5;
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+import OOP.MinkuTante;
 public class Figuras {
+	static String[] objektuPogas = {"Centrs", "Četrsturis", "Aplis", "Trijsturis", "Atgriezties"};
+	
+	static ArrayList<Centrs> centraObjekti = new ArrayList<>();
+	static ArrayList<Cetrsturis> cetrsturaObjekti = new ArrayList<>();
+	static ArrayList<Aplis> aplaObjekti = new ArrayList<>();
+	static ArrayList<Trijsturis> trijsturisObjekti = new ArrayList<>();
 	
 	public static void main(String [] args) {
+		/*
 	//izveido Centrs objektu
 	Centrs centraP1 = new Centrs(-10, 12);
 	System.out.println(centraP1.izvaditKoordinatas());
@@ -23,5 +34,39 @@ public class Figuras {
 	Aplis aplis1 = new Aplis (punkts2, 5);
 	System.out.println(aplis1.izvaditAplaInfo()+"\n"+"s = "+
 	aplis1.rLaukums()+"\n"+"\s"+aplis1.rLGarums());
+	*/
+		
+	 int izvele, poga;
+	
+	do {
+		izvele = MinkuTante.skaitlaParbaude("Ko vēlies darīt?\n"
+				+"0 - Apturēt\n"
+				+"1 - Izveidot objektu\n"
+				+"2 - Aplūkot objektus\n"
+				+"3 - Izsaukt metodi\n",0,3);
+		if(izvele == -1)
+			izvele = 0;
+		switch(izvele) {
+		
+		case 0:
+			JOptionPane.showMessageDialog(null, "Programma apturēta!", "Paziņojums",
+					JOptionPane.INFORMATION_MESSAGE);
+			break;
+			
+		case 1:
+			OIzveide.izveidotObjektu();
+			break;
+			
+		case 2:
+			
+			break;
+			
+		case 3:
+			
+			break;
+			
+		}
+	}while(izvele != 0);
+	
 }
 }
